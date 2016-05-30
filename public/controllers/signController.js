8a589833edc3907fa5cf19d37601da0b3af61e75
praneeth <praneeth7516@gmail.com>
2016-05-29 22:24:12 -0700

    app/routes/signup.js

--- public/controllers/signController.js
+++ public/controllers/signController.js
@@ -38,7 +38,7 @@ angular.module('split').controller('signCntrl', ['$scope', '$http', '$currentVar
 	sign.errorusername = function(username){
 		var result = false;
 		if(username.length > 2 && username.length < 16){
-			var exp = /^[a-zA-Z0-9\s]+$/;
+			var exp = /^([a-zA-Z0-9]+)$/;
 			result = exp.test(username);
 		}else{
 			result = false;
@@ -48,49 +48,68 @@ angular.module('split').controller('signCntrl', ['$scope', '$http', '$currentVar
 	}
 	sign.erroremail = function(email){
 		var result;
-		if(username.length > 2 && username.length < 16){
-			var exp = /^[a-zA-Z0-9\s]+$/;
-			result = exp.test(username);
+		if(email.length > 2 && email.length < 16){
+			var exp = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+			result = exp.test(email);
 		}else{
 			result = false;
 		}
 
-		sign.checkusername = result;
+		sign.checkemail = result;
+	}
+	sign.errorcpassword = function(password, cpassword){
+		var result;
+		if(password == cpassword){
+			result = true;
+		}else{
+			result = false;
+		}
+
+		sign.checkcpassword = result;
 	}
 
 	sign.registerUser = function(){
-		$http.post('/api/signup', sign.regUser).success(function(response){
-			if(response.message == 'Unsuccess'){
-				if(angular.isDefined(response.error.errmsg)){
-					if(response.error.errmsg.indexOf("username") != -1){
+		console.log(sign.cpassword); console.log(sign.regUser.password);
+		if(sign.cpassword == sign.regUser.password){
+			sign.checkcpassword = true;
+			$http.post('/api/signup', sign.regUser).success(function(response){
+				if(response.message == 'Unsuccess'){
+					if(angular.isDefined(response.error.errmsg)){
+						if(response.error.errmsg.indexOf("username") != -1){
+							sign.signupError = true;
+							sign.signupErrorMsg = 'Username already exists!';
+						}
+					    if(response.error.errmsg.indexOf("email") != -1){
+							sign.signupError = true;
+							sign.signupErrorMsg = 'Email id already exists!';
+						}
+					}else{
 						sign.signupError = true;
-						sign.signupErrorMsg = 'Username already exists!';
+						sign.signupErrorMsg = 'Please enter all the fields';
 					}
-				    if(response.error.errmsg.indexOf("email") != -1){
-						sign.signupError = true;
-						sign.signupErrorMsg = 'Email id already exists!';
-					}
-				}else{
-					sign.signupError = true;
-					sign.signupErrorMsg = 'Please enter all the fields';
 				}
-			}
 
-			if(response.message == 'Success'){
-				sign.signupError = false;
-				sign.signup = 'success';
-			}
-		});
+				if(response.message == 'Success'){
+					sign.signupError = false;
+					sign.signup = 'success';
+				}
+			});
+		}else{
+			sign.checkcpassword = false;
+		}
 	}
 
 	sign.login = function(){
 		$http.post('/api/users', sign.loginUser).success(function(response){
 			if(response.message == 'Unsuccess'){
 				sign.signinError = true;
+				sign.signinFail = false;
 				sign.signinErrorMsg = 'Username and/or password is incorrect!';
 			}
 			if(response.message == 'Success'){
 				sign.signin = 'Success';
+				sign.signinError = false;
+				sign.signinFail = false;
 				signInObj = {
 						signIn : true,
 						userExist : true,
@@ -102,6 +121,10 @@ angular.module('split').controller('signCntrl', ['$scope', '$http', '$currentVar
                     user : response.user
                 });
 			}
+		}).error(function(err){
+			sign.signinFail = true;
+			sign.signinError = false;
+			sign.signinFailMsg = 'Service unavailable. Please try again later!';
 		});
 	}
 }]);
